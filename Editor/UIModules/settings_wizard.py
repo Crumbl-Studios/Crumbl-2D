b5d9b89d131f9aa7e7c0ccd010a4e565bc0c8699
bax2004rj <bax2004rj@gmail.com>
2022-05-30 23:52:00 -0400

    added small improvements to settings

--- Editor/UIModules/settings_wizard.py
+++ Editor/UIModules/settings_wizard.py
@@ -1,13 +1,21 @@
 import tkinter
 from tkinter import ttk
+from PIL import ImageTk
 import sv_ttk
 import fileHandler
 import os
+
+settingObjects = ["Personalization","Terminal","Browser","IDE","Asset Viewer","Scene Editor"]
+settingChanged = [False,False,False,False,False,False]
+settingColumns = ["category","changed"]
 
 darkMode = True
 theme = "sun_valley"
 
 class settingsWin():
+    global settingObjects
+    global settingColumns
+    global settingChanged
     def __init__(startAtSection = None):
         self = tkinter.Tk()
         self.geometry("640x640+0+0")
@@ -30,13 +38,24 @@ class settingsWin():
         else:
             if darkMode:
                 ttk.Style().theme_use('alt')
-        self.selectionMenu = ttk.Treeview(self)
-        self.selectionMenu.pack(side = 'left',fill = "y")
+        self.selectionMenu = ttk.Treeview(self,columns=settingColumns)
+        #Load setting categories from list
+        self.selectionMenu.heading("category",text = "Category")
+        self.selectionMenu.pack(side = 'left',fill = "y",expand=0)
         self.settingArea = ttk.Frame(self)
-        self.settingArea.pack(side = 'right',fill = "y")
+        self.settingArea.pack(side = 'right',fill = "both",expand=0)
+        # Bottom bar
+        self.bottomBar = ttk.Frame(self)
+        self.bottomBar.pack(side = "bottom",fill = "x",expand=1)
+        self.okBtn = ttk.Button(self.bottomBar,text = "✓ OK",style="Accent.TButton")
+        self.okBtn.pack(side = "right")
+        self.applyBtn = ttk.Button(self.bottomBar,text = "Apply")
+        self.applyBtn.pack(side = "right")
+        self.closeBtn = ttk.Button(self.bottomBar,text = "⮾ Close")
+        self.closeBtn.pack(side = "right")
         # Personalization settings
         self.personalizedSetting = ttk.Frame(self.settingArea)
-        self.personalizedSetting.pack(fill='both', expand = 1) # Pack this so it shows up on start
+        self.personalizedSetting.pack(fill='both') # Pack this so it shows up on start
         self.personalizationLabel = tkinter.Label(self.personalizedSetting,text="Personalization")
         self.personalizationLabel.pack(side = "top")
         # Terminal settings
--- Editor/fileHandler.py
+++ Editor/fileHandler.py
@@ -8,5 +8,7 @@ IconDir = os.path.join(raw,"UIAssets")
 
 crumbl_logo = os.path.join(IconDir,"favicon.png")
 
+changeImage = os.path.join(IconDir,"changedsetting.png")
+
 run_asset = os.path.join(IconDir,"run.png")
 build_asset = os.path.join(IconDir,"build.png")
