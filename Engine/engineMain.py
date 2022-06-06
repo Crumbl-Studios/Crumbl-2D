8eb51f5ac8475c888272fcf5ca0c08edcbdc75e1
bax2004rj <bax2004rj@gmail.com>
2022-06-06 19:31:01 -0400

    Small refactors and more verbose start sequence

--- Engine/engineMain.py
+++ Engine/engineMain.py
@@ -1,16 +1,16 @@
 from ctypes import cdll
 import os
 
-sdlHandler = cdll.LoadLibrary("./sdlWrapper.so") # COMPILE ENGINE BEFORE RUNNING
-
 class Engine():
+    global sdlHandler
     def __init__(title,xres,yres,fullscreen = False,fullscreenDesk = False,gDriver = 0,
         invisible = False,noDecoration = False,canResize = False,minimized = False,
         maximized = False,foreignWindow = False,highDPI = True,skipTaskbar = False,
         utilWin = False,tooltipWin = False,popup = False):
         print("Starting engine")
+        sdlHandler = cdll.LoadLibrary("./build/sdlWrapper.so") # COMPILE ENGINE BEFORE RUNNING
         sdl = sdlHandler.main(title,xres,yres,fullscreen,fullscreenDesk,gDriver,invisible,noDecoration,
                         canResize,minimized,maximized,foreignWindow,highDPI,skipTaskbar,utilWin,
                         tooltipWin,popup)
-        print("Crumbl engine started")
+        print("Crumbl Engine started")
         return sdl
