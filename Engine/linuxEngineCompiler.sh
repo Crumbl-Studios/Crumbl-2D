4d64d362d2aeba5444c054b37ec32eb40059b41d
bax2004rj <bax2004rj@gmail.com>
2022-06-15 18:13:08 -0400

    Added shell script (linux/unix) to automate compilation

---
+++ Engine/linuxEngineCompiler.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Crumbl engine builder (linux)"
+{
+    cd /Engine/
+}||{
+    echo "Already in directory, compiling"
+}
+gcc -o build/sdlWrapper.so sdlWrapper.cpp -lSDL2 -lSDL2main -lSDL2_ttf -lSDL2_image -export-dynamic -shared -W -fPIC
+echo "Engine built"
+read -p "Would you like to run the test python program? [Yn]" yn
+case $yn in
+    [Yy]* )/bin/python3 engineTester.py;;
+    [Nn]* )exit;; 
+    * )/bin/python3 engineTester.py;;
+esac
