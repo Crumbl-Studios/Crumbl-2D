ff6309dc1de96de640b2c322ab2f1bd22eff95d8
bax2004rj <bax2004rj@gmail.com>
2022-07-22 19:47:47 -0400

    Added quick compile script for code analysis

---
+++ Engine/linuxQuickEngineCompiler.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Quick Engine Build script (Linux/unix)\nDesigned for CodeQL tests"
+{
+    cd ./Engine/
+}||{
+    echo "Already in directory"
+}
+g++ -o build/sdlTest cppTester.cpp -lSDL2 -lSDL2main -lSDL2_ttf -lSDL2_image -shared -W -g -fPIC -Wall -Wpedantic
+echo "Engine built, running test script"
+./build/sdlTest
